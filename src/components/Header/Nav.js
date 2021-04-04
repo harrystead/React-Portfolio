d60e1d336d22bb0c1519f89998fd80dec8f4660e
Harry Stead <harrystead9@gmail.com>
2021-04-04 20:33:46 +0100

    cv added

--- src/components/Header/Nav.js
+++ src/components/Header/Nav.js
@@ -11,7 +11,7 @@ const NavHeader = () => {
             <Link to="/" className="nav-link">Home</Link>
             <Link to="/portfolio" className="nav-link">Portfolio</Link>
             <Link to="/skills" className="nav-link">Skills</Link>
-            <Link to="/skills" className="nav-link">CV</Link>
+            <a href="https://docs.google.com/document/d/1UUzTMyiTXZG_Ljtg5bKZS80TEP703D599g6H0Dc4EtQ/edit?usp=sharing" className="nav-link">CV</a>
           </Nav>
       </Navbar>
     );
